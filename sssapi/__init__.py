6fe15949b6d562692174994f6e3a3f76f1991f71
Jesse Stewart <dj.lnxss@gmail.com>
2020-02-12 10:34:44 -0800

    Updated to use JSON for request.

--- sssapi/__init__.py
+++ sssapi/__init__.py
@@ -10,13 +10,14 @@ class Client(object):
     def get_api_url(self, method):
         return "%s%s" % (self.BASE_URL, method)
     
-    def call(self, data_dict=None, method="stamp"):
+    def call(self, data=None, method="stamp"):
+        json = "{\"data\":\"%s\"}" % (data)
         response = requests.post(self.get_api_url(method=method), 
-                                 data=data_dict, headers={"SnowShoe-Api-Key":self.api_key, "Accept-Encoding":"identity"})
+                                 data=json, headers={"SnowShoe-Api-Key":self.api_key, "Accept-Encoding":"identity", "Content-Type":"application/json"})
         
         if response.status_code >= 500:
             return {"error": "Server error while contacting %s. Data: %s" % (
-                self.get_api_url(method=method), str(data_dict)),
+                self.get_api_url(method=method), str(json)),
                     "code": 20}
         else:
             return response.json()
